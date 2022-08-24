4718afa4343d4cd6aebcf84fa354aed173b06c6a
camilaibar <camuibar@gmail.com>
2022-08-24 18:43:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 console.log('Primer log')
 
-user:{
-name:String,
-surname:String,
-books:Object[],
-pets:String[]
+const user={
+ name:"Camila",
+ surname:"Ibar",
+ books:[],
+ pets:[]
 }
-getFullName():String;
-addPets(String):void;
-countPets():Number;
-addBook(String, String):void;
-getBookNames():String[]
+
+function getFullName() = user.surName + ', ' + user.name;
+function addPets(petName) = user.pets.push(petName);
+function countPets() = user.pets.length();
+function addBook(title, authorName) = user.books.push({name:title, author:authorName});
+function getBookNames() = user.books;
+
+
+addPets("Rocco")
+addPets("Boris")
+
+addBook("Book1", "author1")
+addBook("Book2", "author2")
+addBook("Book3", "author3")
+console.log(getFullName() + " has " + countPets() + "pets (" + user.pets + ") and likes the following books: " + getBookNames())
