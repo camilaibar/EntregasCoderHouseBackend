82067906d6ee5c1aea5fc4436e4bcf4e302f4750
camilaibar <camuibar@gmail.com>
2022-08-24 16:26:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Primer log')
+
+user:{
+name:String,
+surname:String,
+books:Object[],
+pets:String[]
+}
+getFullName():String;
+addPets(String):void;
+countPets():Number;
+addBook(String, String):void;
+getBookNames():String[]
