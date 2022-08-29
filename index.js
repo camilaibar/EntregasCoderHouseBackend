f6c7f11b1544aeba3824b8d1dac330cd032866c8
Camila Ibar <camilaibar@Camilas-MacBook-Air.local>
2022-08-29 12:32:41 -0300

    First challange edited

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-console.log('Primer log')
+console.log("Primer log");
 
-const user={
- name:"Camila",
- surname:"Ibar",
- books:[],
- pets:[]
+const user = {
+  name: "Camila",
+  surname: "Ibar",
+  books: [],
+  pets: [],
+};
+
+function getFullName() {
+  return user.surname + ", " + user.name;
+}
+function addPets(petName) {
+  return user.pets.push(petName);
+}
+function countPets() {
+  return user.pets.length;
+}
+function addBook(title, authorName) {
+  user.books.push({ name: title, author: authorName });
+}
+function getBookNames() {
+  return user.books.map((book) => book.name);
 }
 
-function getFullName() = user.surName + ', ' + user.name;
-function addPets(petName) = user.pets.push(petName);
-function countPets() = user.pets.length();
-function addBook(title, authorName) = user.books.push({name:title, author:authorName});
-function getBookNames() = user.books;
+addPets("Rocco");
+addPets("Boris");
 
+addBook("Book1", "author1");
+addBook("Book2", "author2");
+addBook("Book3", "author3");
 
-addPets("Rocco")
-addPets("Boris")
-
-addBook("Book1", "author1")
-addBook("Book2", "author2")
-addBook("Book3", "author3")
-console.log(getFullName() + " has " + countPets() + "pets (" + user.pets + ") and likes the following books: " + getBookNames())
+console.log(
+  getFullName() +
+    " has " +
+    countPets() +
+    " pets (" +
+    user.pets +
+    ") and likes the following books: " +
+    getBookNames()
+);
