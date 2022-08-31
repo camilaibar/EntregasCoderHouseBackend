dfaef24ba7c79d7623c8bed0dd0e804edd178cd2
Camila Ibar <camilaibar@Camilas-MacBook-Air.local>
2022-08-30 22:58:59 -0300

    Funcionalidad clase dos

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
-console.log("Primer log");
+// Clase 02
+class Product {
+  static id = 0;
+  constructor(productName, productDescription) {
+    this.productID = Product.id++;
+    this.productName = productName;
+    this.productDescription = productDescription;
+  }
 
-const user = {
-  name: "Camila",
-  surname: "Ibar",
-  books: [],
-  pets: [],
-};
-
-function getFullName() {
-  return user.surname + ", " + user.name;
-}
-function addPets(petName) {
-  return user.pets.push(petName);
-}
-function countPets() {
-  return user.pets.length;
-}
-function addBook(title, authorName) {
-  user.books.push({ name: title, author: authorName });
-}
-function getBookNames() {
-  return user.books.map((book) => book.name);
+  getProductID() {
+    return this.productID;
+  }
+  getProductName() {
+    return this.productName;
+  }
+  getProductDescription() {
+    return this.productDescription;
+  }
+  getTotalProductsCounter() {
+    return this.id;
+  }
+  setGlobalCounter(value) {
+    this.id = value;
+  }
 }
 
-addPets("Rocco");
-addPets("Boris");
+let productList = [];
 
-addBook("Book1", "author1");
-addBook("Book2", "author2");
-addBook("Book3", "author3");
+function saveProduct(name, description) {
+  productList.push(new Product(name, description));
+  return productList[productList.lenght - 1];
+}
 
-console.log(
-  getFullName() +
-    " has " +
-    countPets() +
-    " pets (" +
-    user.pets +
-    ") and likes the following books: " +
-    getBookNames()
-);
+function getByID(id) {
+  return productList.find((element) => (element.productID = id));
+}
+
+function deleteByID(id) {
+  productList = productList.filter((element) => element.productID != id);
+}
+
+function deleteAll() {
+  productList[0]?.setGlobalCounter(0); //Reset class static counter
+  productList.length = 0; // Empty array
+}
+
+function printAll() {
+  if (productList.length === 0) {
+    console.log([]);
+    return;
+  }
+  productList.forEach((element) => {
+    console.log(
+      "Product:",
+      element.productID,
+      "Name:",
+      element.productName,
+      "Description:",
+      element.productName
+    );
+  });
+}
+
+console.log("Product list:", productList);
+console.log("Load Product list");
+saveProduct("producto01", "descripcion01");
+saveProduct("producto02", "descripcion02");
+saveProduct("producto03", "descripcion03");
+printAll();
+deleteByID(1);
+console.log("Product list after delete of product 1:");
+printAll();
+deleteAll();
+console.log("Product list after full delete:");
+printAll();
